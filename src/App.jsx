18812bfba527fe3b9bd0ac4da83b21a719276705
rubenbot77 <rubenbot77@hotmail.com>
2024-05-23 16:26:30 -0500

    Adding the parents views of our project called pages and adding routes with useRoutes and BrowserRouter

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App () {
-  return (
-    <>
-      <h1>Shopi</h1>
-    </>
-  )
-}
-
-export default App
---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
+
+  ])
+  return routes
+}
+
+function App () {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
+
+export default App
