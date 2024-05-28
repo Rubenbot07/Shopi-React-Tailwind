cbc9314d4788f8a039df10502db2b9150937d8f0
rubenbot77 <rubenbot77@hotmail.com>
2024-05-28 11:50:28 -0500

    adding api url file, using the fakestore api and adding styles

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,19 +1,19 @@
-export const Card = () => {
+export const Card = ({ id, title, price, description, image, category }) => {
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs px-3 py-0.5 m-2'>Electronics</span>
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs px-3 py-0.5 m-2'>{category}</span>
         <img
-          className='w-full h-full object-cover rounded-lg'
-          src='https://images.pexels.com/photos/1037999/pexels-photo-1037999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''
+          className='w-full h-full object-contain rounded-lg'
+          src={image} alt={description}
         />
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2'>
           +
         </div>
       </figure>
-      <p className='flex justify-between'>
-        <span className='text-sm font-light'>Headphones</span>
-        <span className='text-lg font-medium'>$300</span>
+      <p className='flex justify-between gap-1'>
+        <span className='text-sm font-light h-4 truncate'>{title}</span>
+        <span className='text-lg font-medium'>{`$${price}`}</span>
       </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from 'react'
 import { Layout } from '../../Components/Layout'
 import { Card } from '../../Components/Card'
+import { apiURL } from '../../API'
 
 function Home () {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiURL}/products`)
+        const data = await response.json()
+        setItems(data)
+        console.log(data)
+      } catch (error) {
+        console.log(`Oh no we have an error: ${error}`)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <Layout>
       Home
-      <Card />
+      <section className='grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl p-5'>
+        {
+          items?.map(item => {
+            return (
+              <Card
+                key={item.id}
+                {...item}
+              />
+            )
+          })
+        }
+      </section>
     </Layout>
   )
 }
