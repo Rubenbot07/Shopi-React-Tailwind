efd22ef9329eba6a201a065a1dec22c92e4733ae
rubenbot77 <rubenbot77@hotmail.com>
2024-06-06 14:24:06 -0500

    changing padding in CartMenu

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -23,7 +23,7 @@ export const CheckoutSideMenu = () => {
   }
 
   return (
-    <section className={`${isCartProductsOpen ? 'flex fixed' : 'hidden'} cart flex-col gap-3 bg-white rounded-md w-[95%] max-w-96 right-0 left-0 mx-auto border border-black/40 z-10 h-auto max-h-[80%] overflow-y-auto min-h-48 shadow-lg top-16 px-3 py-6 md:w-[320px] md:right-1 md:max-h-[cal(100vh - 68px)] md:left-auto`}>
+    <section className={`${isCartProductsOpen ? 'flex fixed' : 'hidden'} cart flex-col gap-3 bg-white rounded-md w-[95%] max-w-96 right-0 left-0 mx-auto border border-black/40 z-10 h-auto max-h-[80%] overflow-y-auto min-h-48 shadow-lg top-16 px-4 pt-6 pb-3 md:w-[320px] md:right-1 md:max-h-[cal(100vh - 68px)] md:left-auto`}>
       <span
         className='absolute right-4 cursor-pointer'
         onClick={handleCartProducts}
