b6f0c6095348046b2c14d6865402217e8588130b
rubenbot77 <rubenbot77@hotmail.com>
2024-07-08 17:02:54 -0500

    show password feature done

--- src/Components/SignInForm/index.jsx
+++ src/Components/SignInForm/index.jsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useContext, useRef, useState } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import ReactLoading from 'react-loading'
+import { IoEye, IoEyeOffSharp, IoEyeSharp } from 'react-icons/io5'
 export const SignInForm = () => {
   const {
     signIn,
@@ -34,6 +35,15 @@ export const SignInForm = () => {
       }
     }, '4000')
   }
+  const [showPassword, setShowPassword] = useState(false)
+  const manageShowPassword = () => {
+    setShowPassword(!showPassword)
+    if (passwordRef.current.type === 'password') {
+      passwordRef.current.type = 'text'
+    } else {
+      passwordRef.current.type = 'password'
+    }
+  }
   return (
     <section className='flex flex-col justify-center w-[90%] max-w-96 py-4 min-h-80'>
       <form
@@ -49,15 +59,29 @@ export const SignInForm = () => {
           placeholder='Enter your email address'
           className='border border-gray-400 py-2 px-3 rounded-md text-sm invalid:outline-red-300 dark:text-black'
         />
-        <label htmlFor='password' className='text-sm'>Password</label>
-        <input
-          ref={passwordRef}
-          required
-          id='password'
-          type='password'
-          placeholder='Your password'
-          className='border border-gray-400 py-2 px-3 rounded-md text-sm dark:text-black'
-        />
+        <div className='flex flex-col relative'>
+          <label htmlFor='password' className='text-sm'>Password</label>
+          <div className='flex justify-between items-center gap-1 border border-gray-400 px-2 py-1 rounded-md text-sm dark:text-black mt-2'>
+            <input
+              ref={passwordRef}
+              required
+              id='password'
+              type='password'
+              placeholder='Your password'
+              className='py-1 w-full outline-none'
+            />
+            <span
+              onClick={manageShowPassword}
+              className='text-gray-500 cursor-pointer hover:text-black'
+            >
+              {
+                (!showPassword)
+                  ? <IoEyeOffSharp size='1.2rem' />
+                  : <IoEye size='1.2rem' />
+              }
+            </span>
+          </div>
+        </div>
         <div className={`${invalidUser ? 'flex' : 'hidden'} flex-col items-center text-red-700 bg-red-100 border border-red-500 rounded-md`}>
           <p className='text-lg font-semibold'>Login Failed</p>
           <p>Invalid email or password</p>
